931184322f8c85428e69a8057a50b2044e1ac58a
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-24 21:50:38 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+  var listOfChars = str.split('');
+  var reversedListOfChar = listOfChars.reverse();
+  var reversedString = reversedListOfChar.join('');
+  return reversedString;
+}
+
+function isStringPalindrome(str) {
+  var reversedString = reverseString(str);
+  return str === reversedString;
+}
