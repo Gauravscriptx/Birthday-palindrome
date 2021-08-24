e920b9d65a6a13c6063fd2c7cb61392327df402b
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-24 21:50:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ function isStringPalindrome(str) {
   var reversedString = reverseString(str);
   return str === reversedString;
 }
+function getDateAsString(date) {
+  var dateInStr = { day: "", month: "", year: "" };
+
+  if (date.day < 10) {
+    dateInStr.day = "0" + date.day;
+  } else {
+    dateInStr.day = date.day.toString();
+  }
+
+  if (date.month < 10) {
+    dateInStr.month = "0" + date.month;
+  } else {
+    dateInStr.month = date.month.toString();
+  }
